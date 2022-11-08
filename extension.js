cfff5e5a20f84429164c4ce333ef6c1e17fdb993
Holome <holomeyt@gmail.com>
2022-11-09 00:48:48 +0100

    main

--- extension.js
+++ extension.js
@@ -1,22 +1,69 @@
 const vscode = require('vscode');
-const fs = require('fs');
-/** @param { vscode.ExtensionContext } context */
 
 function activate(context) {
-	console.log("Chiya theme is active !");
+	const fs = require('fs');
+	const paths = {
+		vscode: {
+			root: `${require('path').parse(require('process').execPath).dir}`,
+			wrb: "resources\\app\\out\\vs\\workbench",
+			wdm: "workbench.desktop.main.css"
+		},
+		extension: {
+			root: `${__dirname}`,
+			cbi: "themes\\custom-background-image.css"
+		}
+	}
 
-	/*let refreshCommand = vscode.commands.registerCommand('chiya-theme.refreshTheme', () => {
-		vscode.window.showInformationMessage("The Chiya Theme got refreshed !");
+	/* ---------------------------- */
+	/* Display the background image */
+	/* ---------------------------- */
+	const displayBackgroundImage = vscode.commands.registerCommand('chiya-theme.displayBackgroundImage', () => {
+		// Checks if there is a custom background image
+		if(fs.existsSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\chiya-detector`)) {
+			// Send Message
+			vscode.window.showWarningMessage("Custom background image already exists !");
+		} else {
+			// Edit VSCode CSS & Create a detector file
+			const data = fs.readFileSync(`${paths.extension.root}\\${paths.extension.cbi}`, { encoding: "utf-8", flag: "r" });
+			fs.writeFileSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\${paths.vscode.wdm}`, data, { encoding: "utf-8", flag: "a" });
+			fs.openSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\chiya-detector`, "w");
+
+			// Reload VSCode
+			vscode.commands.executeCommand("workbench.action.reloadWindow");
+
+			// Send Message
+			vscode.window.showInformationMessage("Custom background image created.");
+		}
 	});
+	context.subscriptions.push(displayBackgroundImage);
 
-	context.subscriptions.push(refreshCommand);*/
-}
 
-function deactivate() {
-	console.log("Chiya theme is deactivate !");
+
+	/* -------------------------- */
+	/*  Hide the background image */
+	/* -------------------------- */
+	const hideBackgroundImage = vscode.commands.registerCommand('chiya-theme.hideBackgroundImage', () => {
+		// Checks if there is a custom background image
+		if(fs.existsSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\chiya-detector`)) {
+			// Edit VSCode CSS & Remove the detector file
+			const dataLength = fs.readFileSync(`${paths.extension.root}\\${paths.extension.cbi}`, { encoding: "utf-8", flag: "r" }).length;
+			const data = fs.readFileSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\${paths.vscode.wdm}`, { encoding: "utf-8", flag: "r" });
+			fs.writeFileSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\${paths.vscode.wdm}`, data.slice(0, -dataLength), { encoding: "utf-8"});
+			fs.unlinkSync(`${paths.vscode.root}\\${paths.vscode.wrb}\\chiya-detector`);
+
+			// Reload VSCode
+			vscode.commands.executeCommand("workbench.action.reloadWindow");
+
+			// Send Message
+			vscode.window.showInformationMessage("Custom background image removed.");
+		} else {
+			// Send Message
+			vscode.window.showWarningMessage("Custom background image does not exist yet !");
+		}
+	});
+	context.subscriptions.push(hideBackgroundImage);
 }
 
 module.exports = {
-	activate,
-	deactivate
+	activate
 }
