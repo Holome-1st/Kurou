60d202da1fa1184faa91cbedb2a561ddfa65a1ca
Holome <holomeyt@gmail.com>
2023-01-20 19:40:13 +0100

    2.1.2

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 ### Colors Theme
 ---
-You will find, in this extension, **3 color themes** : Red, Green and Blue.
-All theme colors are **dark mode** and **no light mode** is currently available.
+You will find, in this extension, **3 color themes** : Red, Green and Blue.__
+All theme colors are **dark mode** and **no light mode** is currently available.__
 *By the way, if you're using light mode, you should really go to therapy.*
 
 ### Background image
 ---
-You will also find a feature of custom background image.
-It will be disabled first when you install the extension, but you can enable it easily by following some steps described below.
-- Right here all the steps u have to follow to enable your background image.
-	1. Press the F1 key on your keyboard.
-	2. Write : "Kurou: Change Background Image"
-	3. Press the Enter key on your keyboard.
-	4. Add a direct link to your image (starting with : HTTP or HTTPS, and end with : JPEG or JPG or PNG.
-	5. If nothing changed, please reload VSCode (a cache problem may have occurred).
-- Right here all the steps u have to follow to remove a ugly background image.
-	1. Press the F1 key on your keyboard.
-	2. Write : "Kurou: Remove Background Image"
-	3. Press the Enter key on your keyboard.
-	4. If nothing changed, please reload VSCode (a cache problem may have occurred).
-If your image is installed on your pc and you do not have a web link to it, I recommend this very good free image host : [Postimages](https://postimages.org).
+You will also find **a feature of custom background image**.
+**It will be disabled first** when you install the extension, but **you can enable it** easily by following some steps described below.
+
+- Right here all the steps u have to follow to **enable your background image**.
+	1 - Press the F1 key on your keyboard.
+	2 - Write : "Kurou: Change Background Image"
+	3 - Press the Enter key on your keyboard.
+	4 - Add a direct link to your image (starting with : HTTP or HTTPS, and end with : JPEG or JPG or PNG.
+	5 - If nothing changed, please reload VSCode (a cache problem may have occurred).
+
+- Right here all the steps u have to follow to **remove a ugly background image**.
+	1 - Press the F1 key on your keyboard.
+	2 - Write : "Kurou: Remove Background Image"
+	3 - Press the Enter key on your keyboard.
+	4 - If nothing changed, please reload VSCode (a cache problem may have occurred).
+
+*If your image is installed on your pc and you do not have a web link to it, I recommend this very good free image host : [Postimages](https://postimages.org).*
+
 
 ### Informations
 ---
@@ -29,13 +33,24 @@ Reading the documentation, programming and testing took me about **4/5 working d
 The **2.0.0 of the extension has a totally different functioning from the 1.0.0** (all the code has been modified).
 There is also **no license** for this extension and the code.
 
-### Warnings
+### Errors
 ---
-The extension, for its proper functioning, modifies the files that VSCode uses to work.
-As a result, **VSCode may get corrupted** if the microsoft team changes a large part of the VSCode code.
-If that happens, don't panic, just **reinstall VSCode** and everything will be back to normal.
+You will find below **the list of different errors**, displayed in the logs, which can occur when using the extension.
+
+| Code Error | This error may accurate if : |
+|-------------|------------|
+| **E1** | The extention failed to create the `workbench.background.desktop.main.css` file. |
+| **E2** | The extention failed to edit the `workbench.background.desktop.main.css` file. |
+| **E3** | The extention failed to found or edit the `workbench.html` file. |
+| **E4** | The background URL dont start by `https://` or `http://` and end by `.jpg`, `.jpeg` or `.png`. |
+| **E5** | The extention failed to remove the `workbench.background.desktop.main.css` file. |
+
+**Warning :**
+> The extension, for its proper functioning, modifies the files that VSCode uses to work.
+> As a result, **VSCode may get corrupted** if the microsoft team changes a large part of the VSCode code.
+> If that happens, don't panic, just **reinstall VSCode after removing the extension** and everything will be back to normal.
 
 ### Support
 ---
 To support me, don't forget to give this project a star and share it with those around you !
-*And if you don't want it, it doesn't matter, just click on this link to [help me](https://www.youtube.com/watch?v=dQw4w9WgXcQ) !*
+*And if you don't want it, it doesn't matter, just click on this link to [make me happy](https://www.youtube.com/watch?v=dQw4w9WgXcQ) !*
--- main.js
+++ main.js
@@ -1,17 +1,23 @@
+// Import VSCode API.
 const vscode = require("vscode");
 
+// Import FS and Path API.
 const fs = require("fs");
 const path = require("path");
 
+// Create a Output Channel for extension logs.
 const log = vscode.window.createOutputChannel("Kurou");
-function sendLog(data, jumpLine) {
+function sendLog(data, lastLine) {
 	log.appendLine(data);
-	if(jumpLine) log.appendLine("");
+	if(lastLine) log.appendLine("");
 }
 
+
+// Execute this function when the extension start.
 function activate(context) {
 	sendLog("─ Kurou extension running.", true);
 
+	// Create the workbench.background.desktop.main.css file if doesn't exist.
 	if(!fs.existsSync(path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"))) {
 		try {
 			fs.openSync(path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"), "w");
@@ -20,10 +26,11 @@ function activate(context) {
 		} catch(err) {
 			sendLog("┌ An error has occurred ! [Code: E1]", false);
 			sendLog("├─── Go to the extension's GitHub for more information.", false);
-			sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+			sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 		}
 	}
 
+	// Edit the workbench.background.desktop.main.css file if a background image link is found in the extension settings.
 	const extensionSettings = require("./src/settings.json");
 	const styleFileData = fs.readFileSync(path.join(__dirname, "\\src\\datas\\style.css"), { encoding: "utf-8", flag: "r" });
 	if(extensionSettings.backgroundLink || extensionSettings.backgroundLink != "") {
@@ -37,10 +44,12 @@ function activate(context) {
 		} catch(err) {
 			sendLog("┌ An error has occurred ! [Code: E2]", false);
 			sendLog("├─── Go to the extension's GitHub for more information.", false);
-			sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+			sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 		}
 	}
 
+	// Edit the workbench.html file to include the new tags.
+	// WARNING : This part of the extension is the most sensitive because it modifies the main HTML file of VSCode.
 	try {
 		const htmlWorkbenchData = fs.readFileSync(path.join(vscode.env.appRoot, "\\out\\vs\\code\\electron-sandbox\\workbench\\workbench.html"), { encoding: "utf-8", flag: "r" });
 		const tagsFileData = fs.readFileSync(path.join(__dirname, "\\src\\datas\\tags.html"), { encoding: "utf-8", flag: "r" });
@@ -54,9 +63,10 @@ function activate(context) {
 	} catch(err) {
 		sendLog("┌ An error has occurred ! [Code: E3]", false);
 		sendLog("├─── Go to the extension's GitHub for more information.", false);
-		sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+		sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 	}
 
+	// When the the command executed, change the extension settings file to add the new background image link.
 	const changeBackgroundImage = vscode.commands.registerCommand("kurou.change-background-image", async () => {
 		const newBackgroundLink = await vscode.window.showInputBox({
 			placeHolder: "",
@@ -75,23 +85,24 @@ function activate(context) {
 			try {
 				fs.writeFileSync(path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"), data, { encoding: "utf-8", flag: "w" });
 
-				vscode.window.showInformationMessage("Your wallpaper has been changed !");
+				vscode.window.showInformationMessage("Your wallpaper has been changed, you may need to restart VSCode !");
 				sendLog("─ A file got edited : " + path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"), true);
 			} catch(err) {
 				sendLog("┌ An error has occurred ! [Code: E2]", false);
 				sendLog("├─── Go to the extension's GitHub for more information.", false);
-				sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+				sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 			}
 		} else {
 			sendLog("┌ An error has occurred ! [Code: E4]", false);
 			sendLog("├─── Go to the extension's GitHub for more information.", false);
-			sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+			sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 
 			vscode.window.showWarningMessage("Your link is not supported, please try an another !");
 		}
 	});
 	context.subscriptions.push(changeBackgroundImage);
 
+	// When the the command is executed, change the extension settings file to remove the background image link.
 	const removeBackgroundImage = vscode.commands.registerCommand("kurou.remove-background-image", () => {
 		const settingsData = JSON.parse(fs.readFileSync(path.join(__dirname, "\\src\\settings.json"), { encoding: "utf-8", flag: "r" }));
 		settingsData.backgroundLink = "";
@@ -103,24 +114,26 @@ function activate(context) {
 		try {
 			fs.writeFileSync(path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"), data, { encoding: "utf-8", flag: "w" });
 
-			vscode.window.showInformationMessage("Your wallpaper has been removed !");
+			vscode.window.showInformationMessage("Your wallpaper has been removed, you may need to restart VSCode !");
 			sendLog("─ A file got edited : " + path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"), true);
 		} catch(err) {
 			sendLog("┌ An error has occurred ! [Code: E2]", false);
 			sendLog("├─── Go to the extension's GitHub for more information.", false);
-			sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+			sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 		}
 	});
 	context.subscriptions.push(removeBackgroundImage);
 }
 
+// Execute this function when the extension stop.
 function deactivate() {
+	// Delete the workbench.background.desktop.main.css file.
 	if(fs.existsSync(path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"))) {
 		fs.unlink(path.join(vscode.env.appRoot, "\\out\\vs\\workbench\\workbench.background.desktop.main.css"), (err) => {
 			if(err) {
 				sendLog("┌ An error has occurred ! [Code: E5]", false);
 				sendLog("├─── Go to the extension's GitHub for more information.", false);
-				sendLog("└─── Link : https://github.com/HolomeFR/Kurou", true);
+				sendLog("└─── Link : https://github.com/HolomeFR/Kurou#readme", true);
 			}
 
 			vscode.commands.executeCommand("workbench.action.reloadWindow");
@@ -128,4 +141,5 @@ function deactivate() {
 	}
 }
 
+// Export "activate" and "deactivate" functions.
 module.exports = { activate, deactivate }
